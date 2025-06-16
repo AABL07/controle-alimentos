44363d4282b0bf51ca8db21a644bc0c9df19cf2e
Adriano Lúcio <adrianojunior577@gmail.com>
2025-06-16 01:17:25 -0300

    "Revisão e correção do main.jsx"

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
+// Importa o modo estrito do React para ajudar a detectar problemas de código
 import { StrictMode } from 'react'
+
+// Importa a função para renderizar a aplicação React na DOM
 import { createRoot } from 'react-dom/client'
+
+// Importa o arquivo de estilos globais
 import './index.css'
+
+// Importa o componente principal da aplicação
 import App from './App.jsx'
 
+// Faz o React renderizar o componente <App /> dentro do elemento com id="root" no index.html
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
